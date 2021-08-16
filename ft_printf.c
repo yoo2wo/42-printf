43177a98298c5bbc18f88b52315abeb8e380437f
yooja <wogud758787@naver.com>
2021-08-16 16:46:35 +0900

    2. va test

--- ft_printf.c
+++ ft_printf.c
@@ -6,24 +6,35 @@
 /*   By: jayoo <jayoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 15:14:25 by jayoo             #+#    #+#             */
-/*   Updated: 2021/08/13 15:52:03 by jayoo            ###   ########.fr       */
+/*   Updated: 2021/08/16 16:45:57 by jayoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stdio.h>
 
-int		ft_printf(const char *str, ...);
+int		ft_printf(const char *types, ...);
 
-int		ft_printf(const char *str, ...)
+int		ft_printf(const char *types, ...)
 {
-	int i;
-	va_list ap;
+	va_list ap; //가변 인자 목록
+	int i = 0;
+	int arr[5];
 
-	i = 0;
-	va_start(ap, *str);
-	while (*str != 0)
+	va_start(ap, types); //문자열을 넣으면 문자의 개수를 구해 포인터를 설정한다.
+	while (types[i] != '\0')
 	{
-		str = va_arg(ap, char *);
+		arr[i] = va_arg(ap, int);
+		i++;
 	}
+	printf("%d, %d, %d, %d, %d\n", arr[0], arr[1], arr[2],arr[3],arr[4]);
+	printf("%s", types);
+	va_end(ap);
+	return 0;
+}
 
+int main()
+{
+	ft_printf("start", 1,2,3,4,5);
+	return 0;
 }
